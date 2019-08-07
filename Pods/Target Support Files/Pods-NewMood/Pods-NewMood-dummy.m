adb0ba4e30437310556c965c10d0414752e6d255
Ben Whitley <ben@touchlab.co>
2019-08-06 21:18:37 -0400

    adding pods and workspace - missed in last commit

---
+++ Pods/Target Support Files/Pods-NewMood/Pods-NewMood-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_NewMood : NSObject
+@end
+@implementation PodsDummy_Pods_NewMood
+@end
---
+++ Pods/Target Support Files/Pods-NewMoodTests/Pods-NewMoodTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_NewMoodTests : NSObject
+@end
+@implementation PodsDummy_Pods_NewMoodTests
+@end
